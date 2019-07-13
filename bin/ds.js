2647a69243d1d74fe9d6020cd18b98eb88368e57
Kat Marchán <kzm@zkat.tech>
2019-07-12 19:00:50 -0700

    fix(bin): get entry point working again

--- bin/ds.js
+++ bin/ds.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-require('../src/node/index.js')
-const reqmain = require.main
+const reqmain = require.main;
 require = require('esm')(module, { //eslint-disable-line
   force: true,
   wasm: true,
@@ -11,43 +10,41 @@ require = require('esm')(module, { //eslint-disable-line
     extensions: true,
     vars: true
   }
-})
+});
 
-const CMDS = new Set([
-  'shell',
-])
+require('../lib/node/index.js');
 
-const ALIASES = new Map([
-  ['prep', 'prepare'],
-  ['sh', 'shell']
-])
+const CMDS = new Set(['shell']);
 
-if (reqmain === module) {
-  main(process.argv)
-}
+const ALIASES = new Map([['prep', 'prepare'], ['sh', 'shell']]);
 
-module.exports = main
-function main (argv) {
-  return runCommandWithYargs(argv)
-}
-
-function runCommandWithYargs (argv) {
+function runCommandWithYargs(argv) {
   let config = require('yargs')
     .demandCommand(1, 'Subcommand is required')
     .recommendCommands()
     .help()
     .alias('help', 'h')
     .alias('version', 'v')
-    .completion()
+    .completion();
   if (ALIASES.has(argv[2])) {
-    config = config.command(require(`../src/yargs-modules/${ALIASES.get(argv[2])}.js`))
+    config = config.command(
+      require(`../lib/yargs-modules/${ALIASES.get(argv[2])}.js`)
+    );
   } else if (CMDS.has(argv[2])) {
-    config = config.command(require(`../src/yargs-modules/${argv[2]}.js`))
+    config = config.command(require(`../lib/yargs-modules/${argv[2]}.js`));
   } else {
     for (const mod of CMDS.values()) {
-      config = config.command(require(`../src/yargs-modules/${mod}.js`))
+      config = config.command(require(`../lib/yargs-modules/${mod}.js`));
     }
   }
-  config = config.argv
+  config = config.argv;
 }
 
+function main(argv) {
+  return runCommandWithYargs(argv);
+}
+module.exports = main;
+
+if (reqmain === module) {
+  main(process.argv);
+}
