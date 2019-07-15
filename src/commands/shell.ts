b325665a4319f38fe1a06afecd045d455fa754b9
Andreas Fischer <vengarioth@googlemail.com>
2019-07-15 19:00:40 +0200

    added basic ink setup

--- src/commands/shell.ts
+++ src/commands/shell.ts
@@ -1,15 +1,19 @@
-import figgyPudding, { ProxyFiggyPudding } from "figgy-pudding";
-import { Argv } from "yargs";
+import figgyPudding, { ProxyFiggyPudding } from 'figgy-pudding';
+import { renderApp } from '../components/app';
+import { Argv } from 'yargs';
 
 const ShellOpts = figgyPudding({
   cache: {},
   nodeArg: {},
-  production: {},
+  production: {}
 });
 
-async function shell(argv: Argv, opts: ProxyFiggyPudding<{ cache: {}; nodeArg: {}; production: {} }, {}>) {
+async function shell(
+  argv: Argv,
+  opts: ProxyFiggyPudding<{ cache: {}; nodeArg: {}; production: {} }, {}>
+) {
   opts = ShellOpts(opts);
-  console.error("called `ds sh`:", argv);
+  renderApp(argv, opts);
 }
 
 export default shell;
