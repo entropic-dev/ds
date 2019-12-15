81b85bdff19878603f734d7f6a846fc600b92ef6
Kat Marchán <kzm@zkat.tech>
2019-12-15 15:01:15 -0800

    feat(cmd-shell): load java scripts

--- commands/cmd-shell/src/lib.rs
+++ commands/cmd-shell/src/lib.rs
@@ -1,14 +1,20 @@
+use std::path::PathBuf;
 use std::process::{self, Command, Stdio};
+use std::{env, fs};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
+use directories::ProjectDirs;
 use ds_command::{ArgMatches, Config, DsCommand};
+use ssri::Integrity;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 pub struct ShellCmd {
     #[structopt(long, default_value = "node")]
     node: String,
+    #[structopt(long, hidden = true)]
+    data_dir: Option<PathBuf>,
     #[structopt(multiple = true)]
     args: Vec<String>,
 }
@@ -21,11 +27,23 @@ impl DsCommand for ShellCmd {
                 self.node = node;
             }
         }
+        if args.occurrences_of("data_dir") == 0 {
+            if let Ok(data_dir) = config.get_str("data_dir") {
+                self.data_dir = Some(PathBuf::from(data_dir));
+            }
+        }
         Ok(())
     }
 
     async fn execute(self) -> Result<()> {
         let code = Command::new(self.node)
+            .env(
+                "DS_BIN",
+                env::current_exe()
+                    .context("Failed to get the location of the current ds binary.")?,
+            )
+            .arg("-r")
+            .arg(ensure_dstopic(self.data_dir)?)
             .args(self.args)
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit())
@@ -39,4 +57,25 @@ impl DsCommand for ShellCmd {
         }
         Ok(())
     }
+}
+
+fn ensure_dstopic(dir_override: Option<PathBuf>) -> Result<PathBuf> {
+    let dir = match dir_override {
+        Some(dir) => dir,
+        None => ProjectDirs::from("dev", "entropic", "ds")
+            .ok_or_else(|| anyhow!("Couldn't find home directory."))
+            .context("A home directory is required for ds patch scripts.")?
+            .data_dir()
+            .to_path_buf(),
+    };
+    fs::create_dir_all(&dir)
+        .with_context(|| format!("Failed to create data directory at {:?}", dir))?;
+    let data = include_bytes!("../../../dstopic/dist/dstopic.js").to_vec();
+    let hash = Integrity::from(&data).to_hex().1;
+    let script = dir.join(format!("dstopic-{}", hash.to_string()));
+    if !script.exists() {
+        fs::write(&script, &data)
+            .with_context(|| format!("Failed to write dstopic data file at {:?}", script))?;
+    }
+    Ok(script)
 }
